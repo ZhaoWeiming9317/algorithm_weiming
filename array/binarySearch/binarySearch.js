d80464b02ca5a8dc56b22d9cd62b1b3606c406b5
赵韦铭 <zhaoweiming@192.168.31.43>
2024-11-15 23:56:53 +0800

    binary Search

--- array/binarySearch/binarySearch.js
+++ array/binarySearch/binarySearch.js
@@ -5,6 +5,7 @@
  * 
  * 分为了三种分别是 < value, >= value, > value
  * 其中 bisectLeft 找到 大于等于 value 的下界值
+ * 作用，找到插入的位置，使得插入的元素位于所有相同元素的左侧
  * e.g: 假设要找的 value 为 3
  * [0, 1, 2, 3(bisectLeft), 3, 3, 4(bisectRight), 4, 5]
  * 
@@ -15,6 +16,7 @@
  * @returns {number} - 返回目标值的插入位置（即 >= value 的下界）
  */
 export const bisectLeft = (array, first, last, value) => {
+    // 这个条件确保在找到插入位置时退出循环。
     while (first < last) {
         const mid = first + Math.floor((last - first) / 2); // 修正 mid 的计算方式
         if (array[mid] < value) {
@@ -28,6 +30,7 @@ export const bisectLeft = (array, first, last, value) => {
 
 /**
  * bisect right, 找到大于 value 的下界值
+ * 作用，找到插入的位置，使得插入的元素位于所有相同元素的右侧
  * @param {*} array 
  * @param {*} first 
  * @param {*} last 
@@ -44,4 +47,27 @@ export const bisectRight = (array, first, last, value) => {
         }
     }
     return first;
+}
+
+/**
+ * 标准的二分查找
+ * 作用，找到目标值在数组中的索引
+ * @param {*} array 
+ * @param {*} first 
+ * @param {*} last 
+ * @param {*} value 
+ * @returns 
+ */
+export const binarySearch = (array, first, last, value) => {
+    // 改为 <= 以确保所有元素都被检查
+    while (first <= last) {
+        const mid = first + Math.floor((last - first) / 2)
+        if (array[mid] === value) {
+            return mid;
+        } else if (array[mid] < value) {
+            first = mid + 1;
+        } else {
+            last = mid - 1;
+        }
+    }
 }
